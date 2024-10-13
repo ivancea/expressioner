3e911dec27942fb675c41a2f10793e3c628d8655
Iván Cea Fontenla <ivancea96@outlook.com>
2024-10-13 19:23:05 +0200

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,54 @@
+import DotenvWebpackPlugin from "dotenv-webpack";
+import HtmlWebpackPlugin from "html-webpack-plugin";
+import MiniCssExtractPlugin from "mini-css-extract-plugin";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+export default {
+  entry: "./example/index.ts",
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        use: "ts-loader",
+        include: [
+          path.resolve(__dirname, "lib"),
+          path.resolve(__dirname, "common"),
+        ],
+      },
+      {
+        test: /\.s?css$/,
+        use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
+        include: path.resolve(__dirname, "example/styles"),
+      },
+      {
+        test: /\.(jpg|png)$/,
+        include: path.resolve(__dirname, "example/assets"),
+        type: "asset/resource",
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".tsx", ".ts", ".js"],
+  },
+  output: {
+    filename: "bundle.js",
+    assetModuleFilename: "assets/[hash].[name][ext][query]",
+    path: path.resolve(__dirname, "build"),
+    clean: true,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./example/index.html",
+    }),
+    new MiniCssExtractPlugin(),
+    new DotenvWebpackPlugin({
+      safe: ".env.defaults",
+      defaults: ".env.defaults",
+      systemvars: true,
+    }),
+  ],
+};
